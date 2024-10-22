d4616c860cb988be28f80d83e44796743fba0f81
longzai <longzai.com>
2024-10-22 18:18:06 +0800

    feat-切换主题

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
+'use client'
+
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 import zhCN from 'antd/locale/zh_CN';
 import { ConfigProvider, theme} from 'antd';
 
+import useTheme from '@/hook/useTheme'
+
 import "./globals.css";
+
 
 
 export default function RootLayout({
@@ -11,11 +16,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
-
+  const [curTheme] = useTheme()
+  
   return (
     <html lang="en">
       <body className='h-[100vh]'>
-      <ConfigProvider locale={zhCN} >
+      <ConfigProvider locale={zhCN} theme={{
+        algorithm: curTheme === 'light' ? theme.defaultAlgorithm : theme.darkAlgorithm,
+      }}>
         <AntdRegistry>{children}</AntdRegistry>
       </ConfigProvider>
       </body>
--- src/components/layoutHeader/index.tsx
+++ src/components/layoutHeader/index.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+'use client'
 
+import React from 'react';
+import useTheme from '@/hook/useTheme'
 
 export default function LayoutHeader () {
+
+  const [curTheme, setTheme] = useTheme()
+
+  const changeTheme = () => {
+    console.log(curTheme);
+    
+    setTheme(curTheme==='light' ? 'dark' : 'light')
+  }
+
   return (
     <div className='flex flex-row justify-between h-[50px] shadow-md'>
       <div className='w-[200px] h-[50px] text-center text-4xl text-indigo-600 '>logo</div>
       <div className='flex flex-row mr-[50px]'>
         <div className='ml-[10px]'>用户名</div>
-        <div className='ml-[10px]'>主题</div>
+        <div className='ml-[10px]' onClick={changeTheme}>主题</div>
       </div>
     </div>
   );
