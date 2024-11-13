d374b74a6e55fcf9868e4fe4a1043e0e611e89ec
lz <1793607990@qq.com>
2024-11-13 22:15:12 +0800

    fix-优化esLint配置

--- src/app/(dashboard)/cpm-test/page.tsx
+++ src/app/(dashboard)/cpm-test/page.tsx
@@ -1,31 +1,29 @@
-import React from 'react';
-import { Button } from 'antd';
+import { Button } from "antd";
 
-import LinearCpm from '@/components/linear-cpm';
-import AuthPermission from '@/components/auth-permission';
-import CpmWrap from '@/components/cpm-wrap'
-import DragAndDrop from '@/components/drag-and-drop'
+import AuthPermission from "@/components/auth-permission";
+import CpmWrap from "@/components/cpm-wrap";
+import DragAndDrop from "@/components/drag-and-drop";
+import LinearCpm from "@/components/linear-cpm";
 
 export default function Home() {
   return (
     <div>
       <CpmWrap>
-        <h1 className='mb-2.5'>渐变组件</h1>
+        <h1 className="mb-2.5">渐变组件</h1>
         <LinearCpm></LinearCpm>
       </CpmWrap>
 
       <CpmWrap>
-        <h1 className='mb-2.5'>权限组件</h1>
-        <AuthPermission authCode='add'>
-          <Button type='primary'>新增</Button>
+        <h1 className="mb-2.5">权限组件</h1>
+        <AuthPermission authCode="add">
+          <Button type="primary">新增</Button>
         </AuthPermission>
       </CpmWrap>
-     
+
       <CpmWrap>
-        <h1 className='mb-2.5'>拖拽组件</h1>
+        <h1 className="mb-2.5">拖拽组件</h1>
         <DragAndDrop></DragAndDrop>
       </CpmWrap>
-      
     </div>
   );
 }
